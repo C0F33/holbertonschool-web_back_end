ea89144df55e4a1ef43bfbab18accd712dc268bb
angel bermudez <angelo1998bg@gmail.com>
2024-07-19 19:49:28 -0400

    airport push

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,16 +1,28 @@
-export default class Currency
-{
-    constructor(code, name) {
-        if (typeof code === 'string') {
-            this._code = code;
-        } else {
-            throw TypeError('Code must be a string');
-        }
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
 
-        if (typeof name === 'string') {
-            this._name = name;
-        } else {
-            throw TypeError('name must be a string');
-        }
-    }
+  // Getter and Setter for code
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  // Getter and Setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
