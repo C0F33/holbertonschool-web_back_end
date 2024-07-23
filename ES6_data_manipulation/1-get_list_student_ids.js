3324855f74b8b345f30790d602d29f8488e2aa67
angel bermudez <angelo1998bg@gmail.com>
2024-07-23 19:03:15 -0400

    spaces instead of tabs

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,8 @@
 function getListStudentIds(list) {
-    if (!Array.isArray(list)) {
-        return [];
+  if (!Array.isArray(list)) {
+    return [];
     }
-    return list.map(student => student.id);
+  return list.map(student => student.id);
 }
 
 export default getListStudentIds;
