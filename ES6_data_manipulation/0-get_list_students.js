1ae70cd0a1837690d5e73f2ea02cee0ca1b7d4f0
angel bermudez <angelo1998bg@gmail.com>
2024-07-23 10:37:01 -0400

    00t00

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,15 @@
+const Student = {
+   contructor(id, firstName, location) {
+    this.id = id;
+    this.firstName = firstName;
+    this.location = location;
+    }
+}
+const getListStudents = () => [
+new Student(1, 'Guillaume', 'San Francisco'),
+new Student(2, 'James', 'Columbia'),
+new Student(5, 'Serena', 'San Francisco'),
+];
+
+const students = getListStudents();
+console.log(students);
