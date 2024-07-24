1aa4a6e876934b5e94c0d2a7fb4ace4347ce3fed
angel bermudez <angelo1998bg@gmail.com>
2024-07-23 22:06:24 -0400

    0909

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,17 +1,17 @@
 function cleanSet(set, startString) {
-    if (startString === '' || typeof startString !== 'string') {
-        return '';
+  if (startString === '' || typeof startString !== 'string') {
+    return '';
+  }
+
+  const result = [];
+
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
     }
+  });
 
-    const result = [];
-
-    set.forEach(value => {
-        if (value.startsWith(startString)) {
-            result.push(value.slice(startString.length));
-        }
-    });
-
-    return result.join('-');
+  return result.join('-');
 }
 
 export default cleanSet;
