9009b0a0b9302a75a4fbbf39956edf35e8f59da4
angel bermudez <angelo1998bg@gmail.com>
2024-07-25 21:02:56 -0400

    03t01

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -4,7 +4,7 @@ function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       const [photo, user] = results;
-      console.log(`${photo} ${user.firstName} ${user.lastName}`);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
