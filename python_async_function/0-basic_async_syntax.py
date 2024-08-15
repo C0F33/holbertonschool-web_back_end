cb185d12f232441691c38d7d94fc939a5d9a797b
Angel <angelo1998bg@gmail.com>
2024-08-15 13:48:02 -0400

    00

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Waits for random"""
 import asyncio
 import random
-"""Waits for random"""
 async def wait_random(max_delay: int = 10) -> float:
     """Waits for random delay"""
     delay = random.uniform(0, max_delay)
