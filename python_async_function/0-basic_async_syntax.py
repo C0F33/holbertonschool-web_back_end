c8a76c90b1e58a998998d2be0271c8ce231b894e
Angel <angelo1998bg@gmail.com>
2024-08-15 17:16:59 -0400

    0090

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -2,9 +2,10 @@
 """Waits for random"""
 import asyncio
 import random
+
 async def wait_random(max_delay: int = 10) -> float:
     """Waits for random delay"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
-
     return delay
+
--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -2,7 +2,7 @@
 import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
-"""Coroutines are interesting"""
+"""Take the code from wait_n and alter it into a new function task_wait_n. The code is nearly identical to wait_n except task_wait_random is being called."""
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """COroutines really are"""
     delays = [task_wait_random(max_delay) for _ in range(n)]
