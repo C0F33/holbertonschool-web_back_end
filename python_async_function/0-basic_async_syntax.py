5c5bb3791ee76ddf572634061dc263ac6a4fe1a1
Angel <angelo1998bg@gmail.com>
2024-08-15 13:42:59 -0400

    00

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -3,14 +3,8 @@ import asyncio
 import random
 """Waits for random"""
 async def wait_random(max_delay: int = 10) -> float:
-    """Randomfunc"""
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
+    result = random.uniform(0, max_delay)
+    await asyncio.sleep(result)
 
-if (__name__ == "__main__"):
-    wait_random = __import__('0-basic_async_syntax').wait_random
+    return result
 
-print(asyncio.run(wait_random()))
-print(asyncio.run(wait_random(5)))
-print(asyncio.run(wait_random(15)))
