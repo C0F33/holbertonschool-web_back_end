940eec7f1cda2dc487d3b54573cdc142647c47b1
Angel <angelo1998bg@gmail.com>
2024-08-15 14:57:10 -0400

    989

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -6,7 +6,10 @@ wait_n = __import__ ("1-concurrent_coroutines").wait_n
 def measure_time(n: int, max_delay: int) -> float:
     """Try this"""
     start_time = current_time()
+
     run(wait_n(n, max_delay))
+
     end_time = current_time()
+
     total_time = end_time - start_time
     return total_time / n
