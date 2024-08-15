eca3a2210d4da3329155abf83445cfd51cb863bf
Angel <angelo1998bg@gmail.com>
2024-08-15 18:20:42 -0400

    090

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-"""This function adds two parameters and makes sure they are floats, also makes sure the return is a float"""
-def add(a:float, b:float) -> float:
-    """add"""
+
+""" type-annotated function that takes a float a and
+a float b as arguments and returns their sum as a float. """
+
+
+def add(a: float, b: float) -> float:
+    """ add func """
     return a + b
