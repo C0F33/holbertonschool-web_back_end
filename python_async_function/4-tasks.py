d54ee29e3f3a304045fc3e029a7408a5dc0c37ab
Angel <angelo1998bg@gmail.com>
2024-08-15 17:20:05 -0400

    090

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -4,6 +4,8 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 """Take the code from wait_n and alter it into a new function task_wait_n. The code is nearly identical to wait_n except task_wait_random is being called."""
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """COroutines really are"""
-    delays = [task_wait_random(max_delay) for _ in range(n)]
-    return [await delay for delay in asyncio.as_completed(delays)]
+    """ Wait_n """
+    times: List[float] = []
+    for i in range(n):
+        times.append(task_wait_random(max_delay))
+    return [await i for i in asyncio.as_completed(times)]
