e24df3fbea8b2f124d511830cfead1a116a296c1
Angel <angelo1998bg@gmail.com>
2024-08-15 14:25:33 -0400

    0000

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,8 +2,7 @@
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 """Coroutines"""
-async def wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> list[float]:
     """COroutines"""
     delays = [wait_random(max_delay) for _ in range(n)]
     return [await delay for delay in asyncio.as_completed(delays)]
-
