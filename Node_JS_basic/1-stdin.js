a103c3e9e00eee52a543b69e36c559e68cba684f
C0F33 <a.a.gagot@gmail.com>
2024-09-17 10:16:40 -0400

    00

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,23 +1,20 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
-const question = (query) => {
-    return new Promise((resolve) => rl.question(query, resolve));
-};
+console.log('Welcome to Holberton School, what is your name?');
 
-(async () => {
-    console.log('Welcome to Holberton School, what is your name?');
-    const name = await question('');
-    console.log(`Your name is: ${name}`);
-    rl.close();
-})();
+rl.question('', (name) => {
+
+  console.log(`Your name is: ${name}`);
+
+  rl.close();
+});
 
 rl.on('close', () => {
-    console.log('This important software is now closing');
-    process.exit(0);
-});
 
+  console.log('This important software is now closing');
+});
